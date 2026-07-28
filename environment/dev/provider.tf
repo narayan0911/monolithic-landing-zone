cba96a07f32b4de49ca8ac88eb38069ad2ea72cf
narayan0911 <sultanpurpawan11@gmail.com>
2026-07-29 00:22:06 +0530

    updated

--- environment/dev/provider.tf
+++ environment/dev/provider.tf
@@ -2,19 +2,17 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.81.0"
+      version = "4.76.0"
     }
   }
   backend "azurerm" {
-    resource_group_name  = "stg-rg"
+    resource_group_name  = "mishra_rg"
     storage_account_name = "mishrastorage"
     container_name       = "tfstate"
     key                  = "monolithic.tfstate"
+    }
   }
-}
+  provider "azurerm" {
+    features {}
+  }
 
-
-provider "azurerm" {
-  features {}
-   
-}
